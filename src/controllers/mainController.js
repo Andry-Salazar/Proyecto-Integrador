78cfd4fc008a64dbb2470c5a03003a6ee14c14d1
GabrielGaitanV <gabo13gabrielo@gmail.com>
2023-01-15 22:09:45 -0500

    cambios

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,19 +5,18 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const controller = {
   index: (req, res)=>{
-    const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
-    const Destacado = products.filter((p)=>p.category=="Destacado");
-    const Deporte = products.filter((p)=>p.category=="Deporte");
-    const Hombre = products.filter((p)=>p.category=="Hombre");
-    const Mujer = products.filter((p)=>p.category=="Mujer");
-    const Niño = products.filter((p)=>p.category=="Niño");
+    const destacado = products.filter((p)=>p.category=="Destacado");
+    const deporte = products.filter((p)=>p.category=="Deporte");
+    const hombre = products.filter((p)=>p.category=="Hombre");
+    const mujer = products.filter((p)=>p.category=="Mujer");
+    const niño = products.filter((p)=>p.category=="Niño");
     
     res.render("index",{
-      Destacado: Destacado,
-      Deporte: Deporte,
-      Hombre: Hombre,
-      Mujer: Mujer,
-      Niño: Niño
+      destacado: destacado,
+      deporte: deporte,
+      hombre: hombre,
+      mujer: mujer,
+      niño: niño
     });
   }
 }
