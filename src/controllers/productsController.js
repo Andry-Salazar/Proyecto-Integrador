133c72106dabb71ef50b7d7e55d674d1a0b14d31
GabrielGaitanV <gabo13gabrielo@gmail.com>
2023-01-15 20:54:57 -0500

    arreglo-de-conflictos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,5 @@
 const controller = {
-  detail: (req, res) => res.render('product-detail'),
+  detail: (req, res) => res.render('products/product-detail'),
   create: (req, res) => res.render('create')
 };
 
