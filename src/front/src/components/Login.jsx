2e945f62d95566711cbf5001b7a38e4f8b5af32d
Andry-Salazar <113132734+Andry-Salazar@users.noreply.github.com>
2023-05-08 10:59:39 -0500

    feat: Ajustes (#55)

--- src/front/src/components/Login.jsx
+++ src/front/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Login() {
   return (
-    <div className='message-bar' style={{height:"16rem", background: '#000'}}>
+    <div className='message-bar' style={{height:"16rem"}}>
       Login
     </div>
   );
