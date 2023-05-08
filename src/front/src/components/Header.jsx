50a001b5aaf1ee9a9a3db53d73cc7e40d0aa45ab
Andry-Salazar <113132734+Andry-Salazar@users.noreply.github.com>
2023-05-08 17:05:54 -0500

    feat: Filtrado de productos por categoría (#57)

--- src/front/src/components/Header.jsx
+++ src/front/src/components/Header.jsx
@@ -3,33 +3,118 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="info" style={{ height: "6rem",color:'black', fontWeight: "600", background: 'linear-gradient(45deg, #f08dbd, #52cff3)' }}>
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      bg="info"
+      style={{
+        height: '6rem',
+        color: 'black',
+        fontWeight: '600',
+        background: 'linear-gradient(45deg, #f08dbd, #52cff3)',
+      }}
+    >
       <Container>
-        <Navbar.Brand href="/">
-          <img src="/logo192.png" style={{ width: "140px" }} />
+        <Navbar.Brand>
+          <Link to="/">
+            <img src="/logo192.png" style={{ width: '140px' }} />
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto" style={{	fontSize: "1.1rem" }}>
-            <Nav.Link href="/Productos">Products</Nav.Link>
-            <Nav.Link href="/Usuarios">Users</Nav.Link>
-            <NavDropdown title="Category" id="collasible-nav-dropdown"  style={{	color:'black', fontWeight: "600" }}>
-              <NavDropdown.Item href="/Deportes" style={{	color:'black', fontWeight: "500" }}>Sports</NavDropdown.Item>
-              <NavDropdown.Item href="/Mujeres" style={{	color:'black', fontWeight: "500" }}>Womens</NavDropdown.Item>
-              <NavDropdown.Item href="/Hombres" style={{	color:'black', fontWeight: "500" }}>Men</NavDropdown.Item>
-              <NavDropdown.Item href="/Niños" style={{	color:'black', fontWeight: "500" }}>Children</NavDropdown.Item>
+          <Nav className="me-auto" style={{ fontSize: '1.1rem' }}>
+            <Nav.Link>
+              <Link
+                to="/productos"
+                style={{
+                  textDecoration: 'none',
+                  color: 'var(--bs-navbar-color)',
+                }}
+              >
+                Products
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link
+                to="/usuarios"
+                style={{
+                  textDecoration: 'none',
+                  color: 'var(--bs-navbar-color)',
+                }}
+              >
+                Users
+              </Link>
+            </Nav.Link>
+            <NavDropdown
+              title="Category"
+              id="collasible-nav-dropdown"
+              style={{ color: 'black', fontWeight: '600' }}
+            >
+              <NavDropdown.Item>
+                <Link
+                  to="/productos?category=2"
+                  style={{
+                    textDecoration: 'none',
+                    color: 'var(--bs-navbar-color)',
+                    fontWeight: '500',
+                  }}
+                >
+                  Sports
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link
+                  to="/productos?category=4"
+                  style={{
+                    textDecoration: 'none',
+                    color: 'var(--bs-navbar-color)',
+                    fontWeight: '500',
+                  }}
+                >
+                  Womens
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link
+                  to="/productos?category=3"
+                  style={{
+                    textDecoration: 'none',
+                    color: 'var(--bs-navbar-color)',
+                    fontWeight: '500',
+                  }}
+                >
+                  Men
+                </Link>
+              </NavDropdown.Item>
+              <NavDropdown.Item>
+                <Link
+                  to="/productos?category=5"
+                  style={{
+                    textDecoration: 'none',
+                    color: 'var(--bs-navbar-color)',
+                    fontWeight: '500',
+                  }}
+                >
+                  Children
+                </Link>
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav>
-            <Nav.Link href="/login" style={{ fontSize: "1.1rem", fontWeight: "600" }}>Logout</Nav.Link>
+            <Nav.Link
+              href="/login"
+              style={{ fontSize: '1.1rem', fontWeight: '600' }}
+            >
+              Logout
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
+  );
 }
 export default Header;
